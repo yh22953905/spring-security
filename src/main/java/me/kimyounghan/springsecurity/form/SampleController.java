72ef5aca47d90713e93b444cd863270fe1c0a99a
aims-yhkim <yh.kim@aimskorea.kr>
2020-12-20 23:45:58 +0900

    스프링 시큐리티 - SecurityContextHolder와 Authentication

--- src/main/java/me/kimyounghan/springsecurity/form/SampleController.java
+++ src/main/java/me/kimyounghan/springsecurity/form/SampleController.java
@@ -1,5 +1,7 @@
 package me.kimyounghan.springsecurity.form;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.security.Principal;
 
 @Controller
+@RequiredArgsConstructor
 public class SampleController {
+
+    private final SampleService sampleService;
 
     @GetMapping("/")
     public String index(Model model, Principal principal) {
@@ -28,6 +33,7 @@ public class SampleController {
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
         model.addAttribute("message", "Hello, " + principal.getName());
+        sampleService.dashboard();
         return "dashboard";
     }
 
