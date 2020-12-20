b088eb023af4122de1057fdd5784098f1999b92e
aims-yhkim <yh.kim@aimskorea.kr>
2020-12-20 17:52:56 +0900

    스프링 시큐리티 - 스프링 시큐리티 커스터마이징 : 인메모리 유저 추가

--- src/main/java/me/kimyounghan/springsecurity/config/SecurityConfig.java
+++ src/main/java/me/kimyounghan/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.kimyounghan.springsecurity.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,11 +15,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .mvcMatchers("/", "/info").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .and()
-            .httpBasic()
-        ;
+                .anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+    }
+
+    // 인메모리 유저 추가
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("younghan").password("{noop}1234").roles("USER")
+                    .and()
+                .withUser("admin").password("{noop}1234!").roles("ADMIN");
     }
 }
