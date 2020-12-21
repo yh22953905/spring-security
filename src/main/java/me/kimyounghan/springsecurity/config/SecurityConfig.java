445f19122e6f6d9724cdb3777b1c0a2851bcc379
aims-yhkim <yh.kim@aimskorea.kr>
2020-12-21 13:38:32 +0900

    스프링 시큐리티 - 스프링 시큐리티 필터와 FilterChainProxy

--- src/main/java/me/kimyounghan/springsecurity/config/SecurityConfig.java
+++ src/main/java/me/kimyounghan/springsecurity/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package me.kimyounghan.springsecurity.config;
 import me.kimyounghan.springsecurity.account.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,15 +12,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
-@EnableWebSecurity
+// @EnableWebSecurity // annotation 추가하지 않아도 security 자동 설정이 설정해준다.
+// filter chain을 만들 때 사용된다.
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    AccountService accountService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().authenticated();
