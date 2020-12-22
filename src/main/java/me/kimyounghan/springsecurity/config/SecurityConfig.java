5002d746531c681b8a98a8ea70e1495f64e5c2a8
yh22953905 <yh22953905@gmail.com>
2020-12-22 10:45:30 +0900

    스프링 시큐리티 - CSRF 어택 방지 필터: CsrfFilter favicon 추가

--- src/main/java/me/kimyounghan/springsecurity/config/SecurityConfig.java
+++ src/main/java/me/kimyounghan/springsecurity/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter { // WebSecurit
                 .expressionHandler(securityExpressionHandler());
         http.formLogin();
         http.httpBasic();
+//        http.csrf().disable(); // form 기반의 인증에서는 반드시 사용하는 게 좋다. REST API 의 경우 csrf 토큰을 매번 보내주는 게 번거로울 수 있기 때문에 disabled 시키고 사용할 수 있다.
 
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
